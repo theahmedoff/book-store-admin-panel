cb49e1df4de0a2450786522bb2a0158ded96f637
theahmeoff <aslanakmeof@gmail.com>
2019-03-31 02:29:55 +0400

    Admin Panel upadte

--- src/main/java/book/store/admin/panel/controller/NavigationController.java
+++ src/main/java/book/store/admin/panel/controller/NavigationController.java
@@ -8,11 +8,11 @@ public class NavigationController {
 
     @RequestMapping("/")
     public String indexPage(){
-        return "view/404";
+        return "view/index";
     }
 
-    @RequestMapping("/home")
-    public String homePage(){
+    @RequestMapping("/blank")
+    public String blankPage(){
         return "view/blank";
     }
 
@@ -22,12 +22,55 @@ public class NavigationController {
     }
 
     @RequestMapping("/500")
-    public String Page500(){
+    public String page500(){
         return "view/500";
     }
 
-    @RequestMapping("active-topics")
-    public String activeTopicsPage() {
-        return "view/active-topics";
+    @RequestMapping("/404")
+    public String page404(){
+        return "view/404";
     }
+
+    @RequestMapping("/inbox")
+    public String inboxPage(){
+        return "view/inbox";
+    }
+
+    @RequestMapping("/mail-compose")
+    public String mailComposePage(){
+        return "view/mail-compose";
+    }
+
+    @RequestMapping("/mail-view")
+    public String mailViewPage(){
+        return "view/mail-view";
+    }
+
+    @RequestMapping("/profile")
+    public String proilePage(){
+        return "view/profile";
+    }
+
+    @RequestMapping("/basic-table")
+    public String basicTablePage(){
+        return "view/basic_table";
+    }
+
+    @RequestMapping("/lock-screen")
+    public String lockScreenPage(){
+        return "view/lock_screen";
+    }
+
+    @RequestMapping("/font")
+    public String fontPage(){
+        return "view/font-awesome";
+    }
+
+
+
+
+
+
+
+
 }
